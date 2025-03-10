c9e7dd78bbae2b75397a530e471623f7510b9529
rafa133169 <rafaelmonje1331@gmail.com>
2025-03-09 21:15:16 -0500

    reset

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,30 +1,45 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Repository.Context;
+using Repository.Seeders;
+using Repository.Seeders.SeedersRegister;
+using Service.Logging;
 using Service.Mappings;
 using Service.Register;
-using Service.Register;
+using Service.Services.Contracts;
+using Service.Services.Implementation;
+using Service.SignalR;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuración para aumentar el tamaño máximo del cuerpo de la solicitud en Kestrel - Esto lo agregue porque no me dejaba enviar archivos grandes
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.Limits.MaxRequestBodySize = 100 * 1024 * 1024;
+});
+
+// Configuración para IIS
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.MaxRequestBodySize = 100 * 1024 * 1024;
+});
+
+
 // Add services to the container.
 
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Registrar los servicios
-builder.Services.AddProjectServices();
-
-// Configuracion de la cadena de conexión a la base de datos
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Registrar el contexto de la base de datos
-builder.Services.AddDbContext<DataBaseContext>(options =>
-    options.UseSqlServer(connectionString));
-
-// Mapper
-builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 // Configurar CORS
 var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
@@ -34,11 +49,122 @@ builder.Services.AddCors(options =>
     {
         builder.WithOrigins(allowedOrigins)
                .AllowAnyHeader()
-               .AllowAnyMethod();
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// websocket para el chat
+builder.Services.AddSignalR();
+
+// Registrar los servicios
+builder.Services.AddProjectServices();
+
+// Registrar los seeders
+builder.Services.AddSeeders();
+
+// Registrar el servicio Logger
+builder.Services.AddScoped<Logger>();
+
+// Configuracion de la cadena de conexión a la base de datos
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Registrar el contexto de la base de datos
+builder.Services.AddDbContext<DataBaseContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Configurar Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DataBaseContext>()
+    .AddDefaultTokenProviders();
+
+// Mapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Registrar el seeder
+builder.Services.AddTransient<DataBaseSeeder>();
+
+// Agregar autenticación JWT
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+
+    //  Configurar autenticación con JWT en Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Ingrese el token en el formato: Bearer {token}",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            new string[] {} // Áreas protegidas (vacío significa que aplica a toda la API)
+        }
     });
 });
 
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+// Acceso a user desde la clase de servicio
+builder.Services.AddHttpContextAccessor();
+
+
 var app = builder.Build();
+
+// Poder visualizar las imagenes del Servidor
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+        ctx.Context.Response.Headers.Append("Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept");
+    },
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Uploads")),
+    RequestPath = "/Uploads"
+});
+
+
+// Ejecutar el seeder
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<DataBaseSeeder>();
+    await seeder.SeedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -47,15 +173,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseRouting();
+app.UseCors();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); // Agregar autenticación
 app.UseAuthorization();
 
-// Usar CORS
-app.UseCors();
-
 app.MapControllers();
-
+app.MapHub<ChatHub>("Chathub");
 app.Run();
