46265d97c8ed42c34da63bd9741df18334979d74
rafa133169 <rafaelmonje1331@gmail.com>
2025-03-23 14:56:16 -0500

    ForgotPassword

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string? ResetToken { get; set; }
+        public DateTime? ResetTokenExpiry { get; set; }
+    }
+}
