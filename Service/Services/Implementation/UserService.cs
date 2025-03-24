ce23850aa4fc6ab9b03e2fbe032dd4b5b9d28d1f
rafa133169 <rafaelmonje1331@gmail.com>
2025-03-23 19:06:39 -0500

    RevertAll

--- Service/Services/Implementation/UserService.cs
+++ Service/Services/Implementation/UserService.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Entities;
+using Repository.Context;
 using Service.Services.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+
 
 namespace Service.Services.Implementation
 {
@@ -12,14 +15,13 @@ namespace Service.Services.Implementation
     {
         public async Task<User> GetByEmailAsync(string email)
         {
-            // Implementar lógica para obtener usuario
             return await Task.FromResult(new User { Email = email, PasswordHash = "hashed_password" });
         }
 
         public async Task UpdateAsync(User user)
         {
-            // Implementar lógica para actualizar usuario
             await Task.CompletedTask;
         }
     }
+
 }
